84217fbec24036e28673d77acfb8f13ab4f5fd11
guoleopard@126.com <guoleopard@126.com>
2025-12-02 08:41:01 +0800

    fix icon

--- frontend/src/layout/Layout.vue
+++ frontend/src/layout/Layout.vue
@@ -21,7 +21,6 @@
           :key="item.path"
           :index="item.path"
         >
-          <component :is="item.meta.icon" />
           <template #title>{{ item.meta.title }}</template>
         </el-menu-item>
       </el-menu>
@@ -127,13 +126,13 @@ html, body {
 
 .sidebar-menu .el-menu-item .el-icon {
   margin-right: 8px;
-  font-size: 18px;
+  font-size: 14px;
 }
 
 /* 折叠状态下的菜单图标样式 */
 .sidebar-menu.is-collapse .el-menu-item .el-icon {
   margin-right: 0;
-  font-size: 20px;
+  font-size: 16px;
 }
 
 .main-container {
